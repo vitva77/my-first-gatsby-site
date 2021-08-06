fbc83a884a9c505faf26fdf6f2b81584e5a704d8
vitva77 <mail.own.mail@gmail.com>
2021-08-06 12:35:32 +0300

    Finished Gatsby Tutorial Part 6

---
+++ src/pages/blog/{mdx.slug}.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { graphql } from 'gatsby';
+import { MDXRenderer } from 'gatsby-plugin-mdx'; // highlight-line
+import Layout from '../../components/layout';
+
+const BlogPost = ({ data }) => {
+  // highlight-line
+  return (
+    // highlight-start
+    <Layout pageTitle={data.mdx.frontmatter.title}>
+      <p>{data.mdx.frontmatter.date}</p>
+      <MDXRenderer>{data.mdx.body}</MDXRenderer>
+    </Layout>
+    // highlight-end
+  );
+};
+
+export const query = graphql`
+  query MyQuery($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      body
+    }
+  }
+`;
+
+export default BlogPost;
